cd129718453b7a9f1279e6c9746731d42af42144
Durgesh <sumit.vijayvargiya007@gmail.com>
2022-10-01 02:42:59 +0530

    Initial Commit-4

--- src/test/java/com/javaTechie/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javaTechie/SpringJenkinsApplicationTests.java
@@ -19,7 +19,7 @@ class SpringJenkinsApplicationTests {
 		logger.info("Test Case Executed.............");
 		
 
-		logger.info("Test Case Executed---3================.............");
+		logger.info("Test Case Executed---4================.............");
 		assertEquals(true, true);
 	}
 
